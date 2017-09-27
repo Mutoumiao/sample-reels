22a6dcf1dd61d778bb4ce8ef1e35f1ff1e953189
Mutou <839608583@qq.com>
2017-09-28 04:41:07 +0800

    Update image.js

--- JavaScript/image-preview/image.js
+++ JavaScript/image-preview/image.js
@@ -1,51 +1,30 @@
-
 (function () {
-    //帮助函数
-    function addEvent(node,event,handler){
-        if (!!node.addEventListener){
-            node.addEventListener(event,handler,!1);
-        }else{
-            node.attachEvent('on'+event,handler);
-        }
-    }
-    // =========
     // 高版本浏览器支持
-    function file2dataurl(file,callback){
-        if (!window.FileReader){
-            throw 'Browser not support File API !';
-        }
-        var reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = function(event){
-            callback(event.target.result);
-        };
-    }
-    // 添加预览图片
-    function appendImage(url){
-        // 添加预览图片容器
-        var div = document.createElement('div');
-        div.className = 'itbox';
-        document.getElementById('vbox').appendChild(div);
-        // 添加预览图片
-        var image = new Image();
-        div.appendChild(image);
-        image.src = url;
-    }
-    // 文件选择
-    addEvent(
-        document.getElementById('image'),
-        'change',function(event){
-            if (!event.target){
-                throw 'Browser not support!';
-            }
-            var list = event.target.files;
-            if (!list||!list.length){
-                return;
-            }
-            for(var i=0,l=list.length;i<l;i++){
-                file2dataurl(list[i],appendImage);
-            }
-            event.target.parentNode.reset();
-        }
-    );
+   function filedataUrl(file, callback) {
+       var reader = new FileReader();
+       reader.readAsDataURL(file);
+       reader.addEventListener('load',function(event){
+           callback(event.target.result);
+       });
+   }
+   function appendImage(url) {
+       //添加预览图片容器
+       var div = document.createElement('div');
+       div.classList.add('itbox');
+       document.getElementById('vbox').appendChild(div);
+       //添加预览图片
+       var image = new Image();
+       image.src = url;
+   }
+   function ImageEvent(event) {
+       var list = event.target.file;
+       if(!list || !list.length) {
+           return ;
+       }
+       for (var i = 0, len = list.length; i < len; i++) {
+           filedataUrl(list[i],appendImage);
+           event.target.parentNode.reset();
+       }
+   }
+   document.getElementById('image').addEventListener('change', ImageEvent);
 })();
